cd33277c15a8220c3c4f960554542eb2c9d7c11a
Akinduko <akindukooa@gmail.com>
2020-04-07 00:54:50 +0100

    update settings.

--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -103,6 +103,7 @@ const withdrawFromWallet = async (graph: any): Promise<any> => {
         }
     } = graph;
 
+
     const wallet = await prisma.wallet({ userId });
     if (!wallet) {
         return { success: false };
