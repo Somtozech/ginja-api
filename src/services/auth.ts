63049c81eebbc72768682599070b6ca6f6985719
Akinduko <akindukooa@gmail.com>
2020-04-22 11:07:50 +0100

    updated mail sending

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 // Create auth
+
+import MailTransport from '../core/utils/email';
+
+const mail = new MailTransport();
+
 const createAuth = async (graph: any) => {
     const { args, context } = graph;
     const { prisma } = context;
@@ -28,12 +33,21 @@ const createAuth = async (graph: any) => {
                 }
             }
         });
-
         const user = await prisma.auth({ id: auth.id }).user();
-        await prisma.createUserStatus({ userId: user.id });
         await prisma.updateAuth({ data: { userId: user.id }, where: { id: auth.id } });
         const bank = await prisma.user({ id: user.id }).bank();
-
+        const emailData: any = {
+            to: email,
+            from: 'support@gingabox.com',
+            subject: 'Welcome to Ginjabox',
+            templateId: 'd-c0fbd426b01744e4861a30179cf0e3ee',
+            dynamic_template_data: {
+                full_name: `${firstName} ${lastName}`
+            }
+        };
+        if (bank) {
+            mail.sendMail(emailData);
+        }
         return { ...user, bank };
     } catch (error) {
         throw error;
