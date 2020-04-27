ce736794cfa8249b06300beeed43bca430185dfb
Akinduko <akindukooa@gmail.com>
2020-04-27 06:27:41 +0100

    post rebase cleanup

--- src/services/upload.ts
+++ src/services/upload.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 const cloudinary = require('cloudinary').v2;
 
-const cloudinaryUpload = async (stream): any => {
+const cloudinaryUpload = async (stream: any) => {
     try {
         return await new Promise((resolve, reject) => {
-            const cloudinaryStream = cloudinary.uploader.upload_stream({ tags: 'image_upload' }, function(err, image) {
+            const cloudinaryStream = cloudinary.uploader.upload_stream({ tags: 'image_upload' }, function(err: Error, image: any) {
                 if (err) {
                     reject(err);
                 }
@@ -20,7 +21,7 @@ const cloudinaryUpload = async (stream): any => {
     }
 };
 
-const processUpload = async file => {
+const processUpload = async (file: any) => {
     const { stream } = await file;
     const result = await cloudinaryUpload(stream);
     console.log('In process upload');
